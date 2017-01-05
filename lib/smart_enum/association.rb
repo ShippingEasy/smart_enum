18f30e000e0d836ae277c44ef889496ed9278015
Joshua Flanagan <joshuaflanagan@gmail.com>
2017-01-05 17:10:36 -0600

    Rename SmartEnum.reflections to SmartEnum.enum_associations

--- lib/smart_enum/association.rb
+++ lib/smart_enum/association.rb
@@ -1,8 +1,6 @@
 # Macros for registring associations with other SmartEnum models
 class SmartEnum
   module Association
-    BelongsToReflection = Struct.new(:association_name, :foreign_key, :association_class)
-
     def has_many(association_name, class_name: nil, as: nil, foreign_key: nil, through: nil, source: nil, **opts)
       if opts.any?
         fail "unsupported options: #{opts.keys.join(',')}"
@@ -50,16 +48,18 @@ class SmartEnum
       foreign_key = foreign_key.to_sym
       class_name ||= association_name.to_s.classify
       association_class = class_name.constantize
-      self.reflections[association_name] =
-        BelongsToReflection.new(association_name, foreign_key, association_class)
+      self.enum_associations[association_name] =
+        BelongsToAssociation.new(association_name, foreign_key, association_class)
 
       define_method(association_name) do
         association_class.find_by(id: self.attributes.fetch(foreign_key))
       end
     end
 
-    def reflections
-      @reflections ||= {}
+    def enum_associations
+      @enum_associations ||= {}
     end
+
+    BelongsToAssociation = Struct.new(:association_name, :foreign_key, :association_class)
   end
 end
